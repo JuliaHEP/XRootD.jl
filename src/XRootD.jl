b88bd5c1ab7b11291193c4811e226f8f587434b8
Pere Mato <pere.mato@cern.ch>
2024-05-21 16:48:11 +0200

    changes for linux

--- src/XRootD.jl
+++ src/XRootD.jl
@@ -9,9 +9,9 @@ module XRootD
         include(joinpath(gendir, "jl/XRootD-export.jl"))
         @wrapmodule(()->joinpath(gendir, "build/lib", "libXRootDWrap.$(Libdl.dlext)"))
     else
-        using XRootD_cxxwrap_jll
-        include(XRootD_julia_jll.XRootD_exports)
-        @wrapmodule(()->XRootD_julia_jll.libXRootDWrap)
+        #using XRootD_cxxwrap_jll
+        #include(XRootD_julia_jll.XRootD_exports)
+        #@wrapmodule(()->XRootD_julia_jll.libXRootDWrap)
     end
 
     function __init__()
